b610ae1defea81b47a0a2bfdff1a4ce92ba7d4d4
Randal3 <riccardo.brucato@outlook.it>
2023-01-03 15:29:25 +0100

    cambi nome

--- src/main/java/com/Portalenoinet/Controller/PageController.java
+++ src/main/java/com/Portalenoinet/Controller/PageController.java
@@ -29,11 +29,10 @@ public class PageController {
     private CredentialsService credentialservice;
 
     @Autowired
-	private CredentialsValidator credentialsValidator;
+    private CredentialsValidator credentialsValidator;
 
     @Autowired
-	private UtenteValidator userValidator;
-
+    private UtenteValidator userValidator;
 
     @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
     public String defaultAfterLogin(Model model) {
@@ -41,71 +40,73 @@ public class PageController {
         return "dashboard";
     }
 
-    @GetMapping(value="/index")
+    @GetMapping(value = "/index")
     public String index() {
         return "index";
     }
 
-    @GetMapping(value="/copertura")
+    @GetMapping(value = "/copertura")
     public String copertura(Model model) {
         infoCognomeUtente(model);
         return "copertura";
     }
 
-    @GetMapping(value="/gestioneSim")
+    @GetMapping(value = "/gestioneSim")
     public String gestioneSim(Model model) {
         infoCognomeUtente(model);
         return "gestioneSim";
     }
 
-    @RequestMapping(value =  "/gestioneSim/{areaCode}" , method = RequestMethod.GET)
-     public String gestioneSim(@PathVariable("areaCode") String id, Model model) {
+    @RequestMapping(value = "/gestioneSim/{areaCode}", method = RequestMethod.GET)
+    public String gestioneSim(@PathVariable("areaCode") String id, Model model) {
         model.addAttribute("id", id);
         return "gestioneSim";
-     }
+    }
 
-    @GetMapping(value="/tableSim")
+    @GetMapping(value = "/tableSim")
     public String tableSim(Model model) {
         infoCognomeUtente(model);
         return "tableSim";
     }
-    
-    @GetMapping(value="/gestioneOperatori")
+
+    @GetMapping(value = "/gestioneOperatori")
     public String gestioneOperatori(Model model) {
         infoCognomeUtente(model);
 
         Utente utente = new Utente();
-		Credentials credenziali = new Credentials();
-		model.addAttribute("utente", utente);
-		model.addAttribute("credentials", credenziali);
+        Credentials credenziali = new Credentials();
+        model.addAttribute("utente", utente);
+        model.addAttribute("credentials", credenziali);
         return "gestioneOperatori";
     }
 
-    @RequestMapping(value = "/register" , method = RequestMethod.POST)
-    public String registerUser(@ModelAttribute("utente") Utente utente, BindingResult userBindingResult, @ModelAttribute("credentials") Credentials credenziali, BindingResult credentialsBindingResult, Model model) {
-    
-            infoCognomeUtente(model);
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String registerUser(@ModelAttribute("utente") Utente utente, BindingResult userBindingResult,
+            @ModelAttribute("credentials") Credentials credenziali, BindingResult credentialsBindingResult,
+            Model model) {
 
-            this.userValidator.validate(utente, userBindingResult);
-            this.credentialsValidator.validate(credenziali, credentialsBindingResult);
-            if(!userBindingResult.hasErrors() && !credentialsBindingResult.hasErrors()) {
-                credenziali.setUser(utente);
-                credentialservice.saveCredentials(credenziali);
-                return "dashboard";
-            }
-            return "gestioneOperatori";
+        infoCognomeUtente(model);
+
+        this.userValidator.validate(utente, userBindingResult);
+        this.credentialsValidator.validate(credenziali, credentialsBindingResult);
+        if (!userBindingResult.hasErrors() && !credentialsBindingResult.hasErrors()) {
+            credenziali.setUser(utente);
+            credentialservice.saveCredentials(credenziali);
+            return "dashboard";
+        }
+        return "gestioneOperatori";
     }
 
-    @GetMapping(value="/infoSim")
+    @GetMapping(value = "/infoSim")
     public String infoSim(Model model) {
         infoCognomeUtente(model);
         return "infoSim";
     }
 
-    private void infoCognomeUtente(Model model){
+    private void infoCognomeUtente(Model model) {
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		Credentials credentials = credentialservice.getCredentials(userDetails.getUsername());
-		Utente user = credentials.getUser();
+        Credentials credentials = credentialservice.getCredentials(userDetails.getUsername());
+        Utente user = credentials.getUser();
         model.addAttribute("user", user);
     }
 
